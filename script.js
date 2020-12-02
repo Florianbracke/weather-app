4cbcfb2cc8c6d11c3b4d5b3066874fc5164b49b2
Florianbracke <florianbracke@gmail.com>
2020-12-02 11:15:23 +0100

    add if statement to rainNow variable

--- script.js
+++ script.js
@@ -12,30 +12,33 @@
     <p id="wind"></p */}
 
 function getWeather(){
-
+//variables
 const key = "e83c0d271e5edc4f99d6d218e446d655"; 
 let cityName = "gent";
 let api = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}`;
+//set current date
+// document.getElementById("date").innerHTML = new date;
 
 fetch(api)
     .then(response => response.json())
         .then(data => {
-                let dateMorning = data.list[0].dt_txt
-                let temperatureMorning = (Math.floor(data.list[0].main.temp -273))
-                let minimumtemperatureMorning = (Math.floor(data.list[0].main.temp_min - 273)) 
-                let maximumtemperatureMorning = (Math.floor(data.list[0].main.temp_max - 273))
-                let rainMorning = data  //fix this
-                // console.log(data)
-                // console.log(dateMorning)
-                // console.log(temperatureMorning) 
-                // console.log(minimumtemperatureMorning)
-                // console.log(maximumtemperatureMorning)
-                // console.log(rainMorning);
-                document.getElementById("dateMorning").innerHTML = dateMorning
-                document.getElementById("temperatureMorning").innerHTML = temperatureMorning
-                document.getElementById("minimumtemperatureMorning").innerHTML = minimumtemperatureMorning
-                document.getElementById("maximumtemperatureMorning").innerHTML =maximumtemperatureMorning
-                document.getElementById("rainMorning").innerHTML = rainMorning  //fix this
+            
+                let dateNow = data.list[0].dt_txt
+                let temperatureNow = (Math.floor(data.list[0].main.temp -273))
+                let minimumtemperatureNow = (Math.floor(data.list[0].main.temp_min - 273)) 
+                let maximumtemperatureNow = (Math.floor(data.list[0].main.temp_max - 273))
+                let rainNow = data.list.rain   
+              
+
+                document.getElementById("dateNow").innerHTML = dateNow
+                document.getElementById("temperatureNow").innerHTML = temperatureNow
+                document.getElementById("minimumtemperatureNow").innerHTML = minimumtemperatureNow
+                document.getElementById("maximumtemperatureNow").innerHTML =maximumtemperatureNow
+                document.getElementById("rainNow").innerHTML = rainNow  
+                // if there is no rain
+                if (rainNow == undefined)
+                {document.getElementById("rainNow").innerHTML = "no rain!" }
+                
 
         } )
  }
