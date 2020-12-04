6d6aa8202a781626da6bde4c4f988d2aca8ad6de
Florianbracke <florianbracke@gmail.com>
2020-12-04 15:52:28 +0100

    add timezones and style graph

--- script.js
+++ script.js
@@ -2,24 +2,32 @@
 //variables DOM
 let array = []
 
-
 function getWeather(){  
 
+    //variables 
     cityName = document.querySelector(".city").value
     const key = "e83c0d271e5edc4f99d6d218e446d655"
     let api = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}`
 
-  
-    fetch(api)
+   //fetch data
+   dataSet= fetch(api)
         .then(response => response.json())
             .then(data => {
                 let date = data.list[Number(array.length)].dt_txt
                 let temperatureNow = (data.list[Number(array.length)].main.temp -273);
                 let cloudsNow = (data.list[Number(array.length)].clouds.all);
                 let windspeedNow = (data.list[Number(array.length)].main.temp_max - 273);
-                
+                let timeZone = Number(data.city.timezone)
+                let timeCity = data.list[Number(array.length)].dt
+                let time =  timeCity - timeZone
+                let timeToDate = new Date(time * 1000)
+
+                const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+                let timeLanguage = timeToDate.toLocaleDateString('nl-NL', options);
+
+            
                     // put data into HTML
-                    document.getElementById("date").innerHTML = cityName +  " at " +date;
+                    document.getElementById("date").innerHTML = cityName + ", "  + data.city.country + " at " +timeLanguage;
                     document.getElementById("temperatureNow").innerHTML = "Outside temperature is " + "<strong>" + temperatureNow.toFixed(2) + " </strong>"+ "°C!";
                     document.getElementById("cloudsNow").innerHTML ="The sky is covered for " + "<strong>"+cloudsNow +"</strong>"+ "% with clouds." ;
                     document.getElementById("windspeedNow").innerHTML ="You can expect windspeed at " + "<strong>"+(windspeedNow *3.6).toFixed(2) +"</strong> km/h";
@@ -28,72 +36,19 @@ function getWeather(){
                     if (array.length > 39) {
                         location.reload()}
                     
-                    //place logos into HTML
-                    if (cloudsNow > 100){ 
-                        document.querySelector("body").style.background="url(images/cloudy.jpg)"
-                        document.querySelector("body").style.backgroundSize = "cover" 
-                    } if(windspeedNow > 20){
-                        document.querySelector("body").style.background="url(images/windy.jpg)"
-                        document.querySelector("body").style.backgroundSize = "cover" 
-                    } if (cloudsNow < 50 && temperatureNow > 15){
-                        document.querySelector("body").style.background="url(images/sunnywarm.jpg)"
-                        document.querySelector("body").style.backgroundSize = "cover" 
-                    } if (temperatureNow > 15){
-                        document.querySelector("body").style.background="url(images/verywarm.jpg)"
-                        document.querySelector("body").style.backgroundSize = "cover" 
-                    } if (temperatureNow < 2){
-                        document.querySelector("body").style.background="url(images/verycold.jpg)"
-                        document.querySelector("body").style.backgroundSize = "cover" 
-                    } if(windspeedNow > 20 && temperatureNow > 22){
-                        document.querySelector("body").style.background="url(images/warmwind.jpg)"
-                        document.querySelector("body").style.backgroundSize = "cover" 
-                    }if(temperatureNow < 22 && temperatureNow > 15){
-                        document.querySelector("body").style.background="url(images/oktemp.jpg)"
-                        document.querySelector("body").style.backgroundSize = "cover"   
-                    }
-                    let i = "";
-                    arraydata= [];
-                        for (i = 0; i < 40; i++) {
-                            arraydata.push(Math.floor(Number(data.list[i].main.temp)-273))
-                    }arrayYaxis= [];
-                        for (i = -15; i < 40; i++) {
-                            arrayYaxis.push(i)     
-                    }arrayXaxis=[];
-                        for (i = 0; i < 40; i++) {
-                        arrayXaxis.push(data.list[i].dt_txt)}
-                    
-                        var ctx = document.getElementById("myChart");
-                        Chart.defaults.global.legend.display = false;
-                        var myChart = new Chart(ctx, {
-                            type: 'line', 
-                            data: {
-                                labels: arrayXaxis,
-                                
-                                datasets: [
-                                { 
-                                    data: arraydata,
-                                    borderWidth: 1,
-                                    backgroundColor: "gold",
-                                    label: "temperature",
-                                    borderColor: "white",
-                                    fill: false
-                                }
-                                ]
-                            },
-                            options: {
-                                scales: {
-                                    xAxes: [{gridLines: { color: "rgba(212, 190, 190, 0.664)" }},
-                                            {ticks: {display: false}}],
-                                    yAxes: [{gridLines: { color: "rgba(212, 190, 190, 0.664)" }},
-                                            {ticks: {display: false}}]
-                                    }
-                            }
-                        });
-                })
+                    //place pictures onto HTML
+                   
+            })
+} 
+var i = [];
+setInterval(() => {
+    i.push("boom")
+    if (i.length > 9)
+    {clearInterval}
     
-    }
-
- 
+    document.querySelector("body").style.background=`url(images/${i.length}.jpg)`
+    document.querySelector("body").style.backgroundSize = "cover" 
+}, 12000);
 
 function addThreeHours(){
     array.push("threehours");    
@@ -109,9 +64,75 @@ function minusThreeHours(){
     }  
 }
 
+function graph(){
+
+    //variables 
+    cityName = document.querySelector(".city").value
+    const key = "e83c0d271e5edc4f99d6d218e446d655"
+    let api = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}`
+
+    //fetch data
+    fetch(api)
+        .then(response => response.json())
+            .then(data => {
+                let i = "";
+
+                    //prepare arrays for X- & Y-axis
+                    arraydata= [];
+                        for (i = 0; i < 40; i+=2) {
+                        arraydata.push(Math.floor(Number(data.list[i].main.temp)-273))
+                    }arrayYaxis= [];
+                        for (i = -15; i < 40; i++) {
+                            arrayYaxis.push(i)     
+                    }arrayXaxis=[];
+                        for (i = 0; i < 40; i+=2) {
+                        arrayXaxis.push(data.list[i].dt_txt)}
+
+                        //graph datat + styling
+                        var ctx = document.getElementById("myChart");
+                        
+                        var myChart = new Chart(ctx, {
+                            type: 'line', 
+                            data: {
+                                labels: arrayXaxis,
+                                datasets: [{
+                                    data: arraydata,
+                                    borderWidth: 1,
+                                    backgroundColor: "gold",
+                                    label: "temperature",
+                                    borderColor: "white",
+                                    fill: false }]},
+                            options: {
+                            layout: {padding: {
+                                left: 50,
+                                right: 50,
+                                top: 50,
+                                bottom: 100
+                            }},  
+                            scales: {
+                                xAxes: [{
+                                    display: false,
+                                    gridLines: {
+                                        drawOnChartArea: false,
+                                        tickMarkLength: false,
+                                        drawBorder: false,
+                                        
+                                    }
+                                }],
+                                yAxes: [{
+                                    display: false,
+                                }],
+                            },
+
+                            legend: {
+                                display: false
+                            },
+                        }
+                                         
+                                  
+                            
+                        });
+            }) 
+}
 
 
-
-//variabele x = data.link[i] -> loop op i die telkens + 4 doet
-//loop
-//variabale=  i + 4 && array.push
